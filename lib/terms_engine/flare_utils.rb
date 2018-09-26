cb503dd25333a676c182f2c51882b2dc96c1c23c
Andres Montano <amontano@virginia.edu>
2018-09-26 12:29:45 -0600

    factoring out reindexing code to kmaps engine

--- lib/terms_engine/flare_utils.rb
+++ lib/terms_engine/flare_utils.rb
@@ -1,33 +1,5 @@
 module TermsEngine
-  module FlareUtils
-    INTERVAL = 100
-    START_HOUR=8
-    END_HOUR = 17
-    
-    def self.now
-      Time.now
-    end
-    
-    def self.start_time
-      now = self.now
-      Time.new(now.year, now.month, now.day, START_HOUR)
-    end
-    
-    def self.end_time
-      now = self.now
-      Time.new(now.year, now.month, now.day, END_HOUR)
-    end
-    
-    def self.wait_if_business_hours(daylight)
-      return if daylight.blank?
-      now = self.now
-      end_time = self.end_time
-      if now.wday<6 && self.start_time<now && now<end_time
-        delay = self.end_time-now
-        puts "#{Time.now}: Resting until #{end_time}..."
-        sleep(delay)
-      end
-    end
+  class FlareUtils < KmapsEngine::FlareUtils
     
     def self.reindex_all(options)
       from = options[:from]
@@ -39,7 +11,6 @@ module TermsEngine
       daylight = options[:daylight]
       from_i = from.blank? ? nil : from.to_i
       to_i = to.blank? ? nil : to.to_i
-      view = View.get_by_code('roman.scholar')
       fids = nil
       if (!level.blank? && level<=2) || (level.blank? && letter.blank? && phoneme.blank?)
         if level==1
@@ -76,29 +47,7 @@ module TermsEngine
         fids.select!{|fid| fid >= from_i } if !from_i.nil?
         fids.select!{|fid| fid <= to_i } if !to_i.nil?
       end
-      count = 0
-      current = 0
-      puts "#{Time.now}: Indexing of #{fids.size} terms about to start..."
-      while current<fids.size
-        limit = current + INTERVAL
-        limit = fids.size if limit > fids.size
-        self.wait_if_business_hours(daylight)
-        sid = Spawnling.new do
-          puts "Spawning sub-process #{Process.pid}."
-          fids[current...limit].each do |fid|
-            f = Feature.get_by_fid(fid)
-            if f.index
-              name = f.prioritized_name(view)
-              puts "#{Time.now}: Reindexed #{name.name if !name.blank?} (#{f.fid})."
-            else
-              puts "#{Time.now}: #{f.fid} failed."
-            end
-          end
-          Feature.commit
-        end
-        Spawnling.wait([sid])
-        current = limit
-      end
+      self.reindex_fids(fids, daylight)
     end
   end
 end
