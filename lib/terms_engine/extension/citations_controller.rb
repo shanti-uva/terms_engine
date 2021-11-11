a44f7c07923b58e777f2e3562140c89f20df4ecb
Andres Montano <amontano@virginia.edu>
2021-11-10 18:42:58 -0600

    displaying tabs only if necessary

--- lib/terms_engine/extension/citations_controller.rb
+++ lib/terms_engine/extension/citations_controller.rb
@@ -6,7 +6,11 @@ module TermsEngine
       included do
         # This is an extension of the END USER citations display. Make sure there are the corresponding routes, controllers and views.
         belongs_to :descriptions, :feature_geo_code, :feature_name, :feature_name_relation, :feature_relation, #This list comes from the CitationsController in kmaps_engine
-        :passage, :passage_translation, :translation_equivalent, :definition_association #specific to places_engine
+        :definition, :definition_association, :passage, :passage_translation, :translation_equivalent #specific to terms_engine
+      end
+      
+      def collection
+        @collection ||= parent_object.citations.where.not(info_source_type: :InfoSource)
       end
     end
   end
