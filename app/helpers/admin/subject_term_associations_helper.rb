73e34efd8c419dcc7a883562186676fd9d3237df
Aimee Kvasir <aimee@splay.com>
2024-01-22 13:20:14 -0800

    MANU-7849 remove the word association from the starter subject term associations list in accordion

--- app/helpers/admin/subject_term_associations_helper.rb
+++ app/helpers/admin/subject_term_associations_helper.rb
@@ -1,7 +1,7 @@
 module Admin::SubjectTermAssociationsHelper
   def new_subject_term_associations_links(term)
     SubjectTermAssociation.select('branch_id').distinct.sort_by {|a| a.branch['header']}.collect do |a|
-      link_to "#{a.branch['header']} association", new_admin_feature_subject_term_association_path(term, branch_id: a.branch_id)
+      link_to "#{a.branch['header']}", new_admin_feature_subject_term_association_path(term, branch_id: a.branch_id)
     end.join(" | ").html_safe
   end
 end
