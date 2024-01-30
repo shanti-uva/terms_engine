1a8dbbec88094e5bee41d34f95c278709300c6b7
Aimee Kvasir <aimee@splay.com>
2024-01-29 19:52:48 -0800

    MANU-7851 redirect form submissions to accordion with section expanded

--- app/controllers/admin/subject_term_associations_controller.rb
+++ app/controllers/admin/subject_term_associations_controller.rb
@@ -5,7 +5,8 @@ class Admin::SubjectTermAssociationsController < AclController
   belongs_to :feature
 
   new_action.before { object.branch_id = params[:branch_id] if params[:branch_id] }
-
+  create.wants.html { redirect_to admin_feature_path(object.feature_id) + '?section=subjectAssociations' } 
+  update.wants.html { redirect_to admin_feature_path(object.feature_id) + '?section=subjectAssociations' } 
   protected
 
   # Only allow a trusted parameter "white list" through.
