72952d1ab2ad7dfb6e6d2ded84bc1428989c4a2c
Aimee Kvasir <aimee@splay.com>
2024-02-23 12:05:36 -0800

    MANU-7891 expand citations section after crud

--- lib/terms_engine/extensions/admin_citations_controller.rb
+++ lib/terms_engine/extensions/admin_citations_controller.rb
@@ -6,7 +6,13 @@ module TermsEngine
       included do
         belongs_to :caption, :description, :feature, :feature_geo_code, :feature_name, :feature_name_relation, :feature_relation, :summary, #This list comes from the CitationsController in kmaps_engine
         :definition, :passage, :subject_term_association #specific to terms_engine
+
+        create.wants.html  { redirect_to polymorphic_url(helpers.stacked_parents, section: 'citations') }
+        update.wants.html  { redirect_to polymorphic_url(helpers.stacked_parents, section: 'citations') }
+        destroy.wants.html { redirect_to polymorphic_url(helpers.stacked_parents, section: 'citations') }
+
       end
+      
     end
   end
 end
