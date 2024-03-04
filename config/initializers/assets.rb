d61c3daa6be96a2112e45773d745aebcb8f118fd
Aimee Kvasir <aimee@splay.com>
2024-03-04 12:58:57 -0800

    MANU-7891 js to link breadcrumbs back to feature_name accordion sections

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,6 @@ Rails.application.config.assets.precompile.concat(['terms_engine/recordings.js',
                                                    'terms_engine/feature_relation_admin.js',
                                                    'terms_engine/related-section-initializer.js',
                                                    'terms_engine/features_admin_accordion.js',
-                                                   'terms_engine/definitions_admin_accordion.js'
+                                                   'terms_engine/definitions_admin_accordion.js',
+                                                   'terms_engine/feature_names_admin_accordion.js'
                                                    ])
