f2e514acaa6011f870a078290a2b9802b27e481d
Aimee Kvasir <aimee@splay.com>
2024-05-09 19:53:21 -0700

    MANU-7917 redirect CRUD for passage translation to appropriate page

--- app/controllers/admin/passage_translations_controller.rb
+++ app/controllers/admin/passage_translations_controller.rb
@@ -16,6 +16,28 @@ class Admin::PassageTranslationsController < AclController
     @languages = Language.order('name')
   end
 
+  create.wants.html do
+    if object.context.instance_of? Definition
+      redirect_to polymorphic_url( [:admin, object.context.feature, object.context], section: 'passage_translations' )
+    else
+      redirect_to polymorphic_url( [:admin, object.context], section: 'passage_translations' )
+    end
+  end
+  update.wants.html do
+    if object.context.instance_of? Definition
+      redirect_to polymorphic_url( [:admin, object.context.feature, object.context], section: 'passage_translations' )
+    else
+      redirect_to polymorphic_url( [:admin, object.context], section: 'passage_translations' )
+    end
+  end
+  destroy.wants.html do
+    if object.context.instance_of? Definition
+      redirect_to polymorphic_url( [:admin, object.context.feature, object.context], section: 'passage_translations' )
+    else
+      redirect_to polymorphic_url( [:admin, object.context], section: 'passage_translations' )
+    end
+  end
+
   protected
 
   # Only allow a trusted parameter "white list" through.
