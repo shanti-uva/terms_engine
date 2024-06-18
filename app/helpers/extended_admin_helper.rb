03168a1d8f9388553cc0bf615f4bc1ae8a934a6c
Aimee <aimee@splay.com>
2024-06-18 10:01:11 -0700

    MANU-7891 Create an extended breadcrumb method in terms app; remove terms specific items from kmaps app (#87)

--- app/helpers/extended_admin_helper.rb
+++ app/helpers/extended_admin_helper.rb
@@ -28,4 +28,46 @@ module ExtendedAdminHelper
     array << parent_object
     array
   end
+
+  def extended_add_breadcrumb_base
+    # Notes and Citations are polymorphic. Support breadcrumbs for each of the parent types!
+    add_breadcrumb_item feature_link(contextual_feature)
+    case parent_type
+    when :passage_translation # in terms_engine
+      add_breadcrumb_item link_to(PassageTranslation.model_name.human(:count => :many).s, polymorphic_path([:admin, parent_object.feature, parent_object.context], section: 'passage_translations'))
+      add_breadcrumb_item link_to(parent_object.content.strip_tags.truncate(25).s,  polymorphic_path([:admin, parent_object]))
+    when :passage # in terms_engine
+      if parent_object.context.instance_of? Feature
+        add_breadcrumb_item link_to(Passage.model_name.human(:count => :many).s, admin_feature_path(parent_object.feature, section: 'passages'))
+        add_breadcrumb_item link_to(parent_object.content.strip_tags.truncate(25).s, polymorphic_path([:admin, parent_object.feature, parent_object]))
+      else
+        add_breadcrumb_item link_to(Passage.model_name.human(:count => :many).s, polymorphic_path([:admin, parent_object.feature, parent_object.context], section: 'passages'))
+        add_breadcrumb_item link_to(parent_object.content.strip_tags.truncate(25).s, polymorphic_path([:admin, parent_object.context, parent_object]))
+      end
+    when :definition # in terms_engine
+      add_breadcrumb_item link_to(Definition.model_name.human(:count => :many).s, admin_feature_path(object.feature.fid, section: 'definitions'))
+      add_breadcrumb_item link_to(parent_object.content.strip_tags.truncate(25).s, admin_feature_definition_path(object.feature, parent_object, section: 'citations'))
+    when :description
+      add_breadcrumb_item feature_descriptions_link(parent_object.feature)
+      add_breadcrumb_item link_to(parent_object.title.strip_tags.truncate(25).titleize.s, admin_feature_description_path(parent_object.feature, parent_object))
+    when :feature
+    when :feature_name
+      add_breadcrumb_item link_to(FeatureName.model_name.human(count: :many).titleize.s, admin_feature_path(parent_object.feature.fid, section: 'names'))
+      add_breadcrumb_item link_to(parent_object.name.strip_tags.truncate(25).s, admin_feature_name_path(parent_object))
+    when :feature_name_relation
+      add_breadcrumb_item feature_names_link(parent_object.child_node.feature.fid)
+      add_breadcrumb_item link_to(parent_object.child_node.name, admin_feature_name_path(parent_object.child_node))
+      add_breadcrumb_item link_to(ts('relation.this', :count => :many), admin_feature_name_feature_name_relations_path(parent_object.child_node))
+      add_breadcrumb_item link_to(parent_object, admin_feature_name_feature_name_relation_path(parent_object.child_node, parent_object))
+    when :feature_geo_code
+      add_breadcrumb_item link_to(FeatureGeoCode.model_name.human(:count => :many).s, admin_feature_feature_geo_codes_path(parent_object.feature))
+      add_breadcrumb_item link_to(parent_object, admin_feature_geo_code_path(parent_object))
+    when :feature_relation
+      add_breadcrumb_item link_to(ts('relation.this', :count => :many), admin_feature_feature_relations_path(parent_object.child_node))
+      add_breadcrumb_item feature_relation_role_label(parent_object.child_node, parent_object, :use_first=>false)
+    when :time_unit
+      add_breadcrumb_item link_to(ts('date.this', :count => :many), admin_time_units_path)
+      add_breadcrumb_item link_to(parent_object.to_s, polymorphic_path([:admin, parent_object]))
+    end
+  end
 end
