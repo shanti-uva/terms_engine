0120d12cf723519153c8d3f3831cea21166d2128
Aimee Kvasir <aimee@splay.com>
2024-09-19 14:41:57 -0700

    MANU-7297 translations, models, views, controllers for enumerations

--- app/models/enumeration.rb
+++ app/models/enumeration.rb
@@ -1,7 +1,7 @@
 class Enumeration < ApplicationRecord
   
-  belongs_to :feature, touch: true
-  validates_presence_of :feature_id, :value
+  belongs_to :context, polymorphic: true, touch: true # Feature, Definition
+  validates_presence_of :context_id, :context_type, :value
 
 end
   
