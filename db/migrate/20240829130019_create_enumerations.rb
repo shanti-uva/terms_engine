f6fb9ea86dd855ea09435816d267e47be9bdc70b
Aimee Kvasir <aimee@splay.com>
2024-09-19 14:51:10 -0700

    MANU-7297 migrations for enumerations

--- db/migrate/20240829130019_create_enumerations.rb
+++ db/migrate/20240829130019_create_enumerations.rb
@@ -1,7 +1,8 @@
 class CreateEnumerations < ActiveRecord::Migration[5.2]
   def change
     create_table :enumerations do |t|
-      t.integer :feature_id, null: false
+      t.integer :context_id, null: false
+      t.string  :context_type, null: false
       t.integer :value, null: false
       
       t.timestamps
--- db/migrate/20240902123011_move_numerology_to_enumerations.rb
+++ db/migrate/20240902123011_move_numerology_to_enumerations.rb
@@ -2,7 +2,7 @@ class MoveNumerologyToEnumerations < ActiveRecord::Migration[5.2]
   def change
     ActiveRecord::Base.transaction do
       Definition.where("numerology IS NOT NULL").each do |d|
-        e = Enumeration.new(:feature_id => d.feature_id, :value => d.numerology)
+        e = Enumeration.new(:context_id => d.id, :context_type => 'Definition', :value => d.numerology)
         e.save
       end
     end
