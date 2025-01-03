6059047b2b5a4e46b0d9e0602d0f54215f7aaa9d
Andres Montano <amontano@virginia.edu>
2025-01-03 09:51:56 -0600

    adding annotations to enumeration

--- app/models/enumeration.rb
+++ app/models/enumeration.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: enumerations
+#
+#  id           :bigint           not null, primary key
+#  context_type :string           not null
+#  value        :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  context_id   :integer          not null
+#
 class Enumeration < ApplicationRecord
   
   belongs_to :context, polymorphic: true, touch: true # Feature, Definition
