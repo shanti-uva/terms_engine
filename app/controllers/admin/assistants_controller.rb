f34769ff6e177c7ec044a7988adc983db4f29419
Andres Montano <amontano@virginia.edu>
2025-01-30 17:04:41 -0600

    refining assistant

--- app/controllers/admin/assistants_controller.rb
+++ app/controllers/admin/assistants_controller.rb
@@ -4,27 +4,16 @@ class Admin::AssistantsController < AclController
   
   # GET /assistant . No id because it is a singular resource (resource :assistant)
   def show
-    tibetan = Language.get_by_code('bod')
-    @dictionaries = InfoSource.where(language_id: tibetan.id).order(:position)
-    @languages = Language.all.order(:name)
+    set_variables
     @assistant = Assistant.new
-    @prompt = ''
-    @translations = {}
-    @terms = []
-    @definitions = []
   end
   
   # POST /assistants
   def create
-    tibetan = Language.get_by_code('bod')
-    @dictionaries = InfoSource.where(language_id: tibetan.id).order(:position)
-    @languages = Language.all.order(:name)
+    set_variables
     @assistant = Assistant.new(assistant_params)
     dicts = @assistant.base_dictionary.nil? ? 1 : @assistant.base_dictionary.code
     parsed_text = ShantiIntegration::TranslationTool.translate(@assistant.passage, dicts: dicts)
-    @terms = []
-    @fids = []
-    @definitions = []
     parsed_text['words'].each do |term, defs|
       next if defs.empty?
       @terms << CGI.unescapeHTML(term)
@@ -46,7 +35,10 @@ class Admin::AssistantsController < AclController
     else
       @prompt = "When translating the passage \"#{@assistant.passage.gsub(/[\n\v\r]/, " ")}\" into #{@assistant.language.name} like as \"#{@assistant.translated_passage.gsub(/[\n\v\r]/, " ")}\""
       @prompt << "How were the following Tibetan terms exactly translated? " + @terms.join(", ")
-      @prompt << "Give response as a JSON object with the keys being all terms exactly as they were provided and the value its corresponding translation without any additional text or explanation. Put verbs in infinitive."
+      @prompt << " Give response as a JSON object with the keys being all terms exactly as they were provided and the value and array."
+      @prompt << " The first element of the array should be the corresponding translation without any additional text or explanation."
+      @prompt << " If verb is conjugated in past, present or future tense, put in third person singular. Nouns and adjectives put in masculine singular."
+      @prompt << " In the second element of the array put the grammatical function among the following options: #{@grammatical_functions.keys.join(', ')} (which gets translated into a preposition)."
       # Initialize OpenAI client
       client = OpenAI::Client.new(access_token: current_user.access_token)
       # Call ChatGPT API
@@ -67,9 +59,19 @@ class Admin::AssistantsController < AclController
   end
   
   private
-    # Only allow a list of trusted parameters through.
-    def assistant_params
-      params.require(:assistant).permit(:passage, :translated_passage, :base_dictionary_id, :destination_dictionary_id, :language_id)
-    end
   
+  def set_variables
+    tibetan = Language.get_by_code('bod')
+    @dictionaries = InfoSource.where(language_id: tibetan.id).order(:position)
+    @languages = Language.all.order(:name)
+    @terms = []
+    @fids = []
+    @definitions = []
+    @grammatical_functions = {'verb' => 1, 'noun' => 2, 'pronoun' => 3, 'adjective' => 4, 'adverb' => 5, 'postposition' => 6}
+  end
+  
+  # Only allow a list of trusted parameters through.
+  def assistant_params
+    params.require(:assistant).permit(:passage, :translated_passage, :base_dictionary_id, :destination_dictionary_id, :language_id)
+  end
 end
